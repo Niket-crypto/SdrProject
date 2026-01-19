8ebfdb894a25d3cebaf6d005461e27d56502a7d6
Niket-crypto <0.nikitasergienko.0@gmail.com>
2026-01-19 02:11:40 +0200

    Replace Name property with Description in MyData

--- ServerApp/Models/MyData.cs
+++ ServerApp/Models/MyData.cs
@@ -3,6 +3,6 @@ namespace ServerApp.Models
     public class MyData
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = "Default Description";
     }
 }
