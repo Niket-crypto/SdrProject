2f905d7a238cab227b6f503de7e5417b72a94878
Niket-crypto <0.nikitasergienko.0@gmail.com>
2026-01-19 14:17:14 +0200

    Update MyData.cs

--- ServerApp/Models/MyData.cs
+++ ServerApp/Models/MyData.cs
@@ -2,7 +2,6 @@ namespace ServerApp.Models
 {
     public class MyData
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = "Clean Lab 6 Data";
+        public string Name { get; set; } = string.Empty;
     }
 }
