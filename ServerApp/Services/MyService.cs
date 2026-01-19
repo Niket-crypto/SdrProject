8c7bed9c35080ce9672bbec1cc79078f935adfb8
Niket-crypto <0.nikitasergienko.0@gmail.com>
2026-01-19 02:21:37 +0200

    Update MyService.cs

--- ServerApp/Services/MyService.cs
+++ ServerApp/Services/MyService.cs
@@ -3,6 +3,6 @@ namespace ServerApp.Services
     public class MyService 
     { 
         
-        public string GetStatus() => "Service is active";
+        public string GetStatus() => "Service is running";
     }
 }
